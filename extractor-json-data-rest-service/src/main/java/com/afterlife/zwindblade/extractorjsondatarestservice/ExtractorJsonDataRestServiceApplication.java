04cb6bfe24ec8b51112ec5a86120cd2cdabd3408
zWindBlade <gabreex@hotmail.com>
2019-11-06 21:03:40 -0400

    Initial commit.
    Initial commit.

---
+++ extractor-json-data-rest-service/src/main/java/com/afterlife/zwindblade/extractorjsondatarestservice/ExtractorJsonDataRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.afterlife.zwindblade.extractorjsondatarestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractorJsonDataRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractorJsonDataRestServiceApplication.class, args);
+	}
+
+}
